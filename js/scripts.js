a194d2bcbb5ff7f4d4a9213ffed93779f87d6cee
angelanmorlan <angelanmorlan@eagles.ewu.edu>
2017-05-17 22:43:44 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-// Scripts
-$("document").ready(function() {
-  var options = {  videoId: 'x6DC1rAnZIg', 
-                   start: 0, 
-                   repeat: true
-                };  
-  
-	$('#video').tubular({mute: false}); 
-});
+
 
     
